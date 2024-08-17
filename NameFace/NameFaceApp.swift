ae9456b1c5ae760078ea9be2009c092f6a293a11
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-17 08:00:56 +0200

    Initial Commit

---
+++ NameFace/NameFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameFaceApp.swift
+//  NameFace
+//
+//  Created by Adam Elfsborg on 2024-08-17.
+//
+
+import SwiftUI
+
+@main
+struct NameFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
